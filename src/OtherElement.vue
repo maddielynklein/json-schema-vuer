a083726191709853dca260de3eb1ee01ae88977d
Maddie <madisonklein@mymail.mines.edu>
2019-10-14 18:57:56 -0600

    Adding enum logic

--- src/OtherElement.vue
+++ src/OtherElement.vue
@@ -29,16 +29,35 @@
         <span v-if="element.examples" class="vueml-json-examples">examples: {{element.examples.toString()}}</span>
 
         <template v-if="!constantValue">
+          <template v-if="getHasEnum(element)">
+            <CollapsibleElement type="array" :initiallyCollapsed="true">
+              <template v-slot:title><span>Enum:</span></template>
+              <template v-slot:content>
+                <span v-for="eVal in element.enum" v-bind:key="eVal">{{ isString ? ('"' + eVal + '"') : eVal.toString() }}</span>
+              </template>
+            </CollapsibleElement>
+          </template>
           <template v-for="combo in combinationKeys">
             <span v-bind:key="combo+'-label'" v-if="element[combo] && element[combo].length > 0">{{ combo }}:</span>
             <div v-bind:key="combo" class="vueml-json-details">
-              <span v-for="(option,index) in element[combo]" v-bind:key="index">
-                <template v-for="value in getFormattedValues(option)" >
-                  <span v-bind:key="value">{{ value }}</span>
-                </template>
-                <template v-for="key in extraNestedElementKeys" >
-                  <span v-bind:key="key" v-if="key == 'pattern' && option[key] != null"><code>{{option[key]}}</code></span>
-                  <span v-bind:key="key" v-else-if="option[key] != null">{{option[key]}}</span>
+              <span v-for="(option,index) in element[combo]" v-bind:key="index" class="vueml-json-conditional">
+                <template v-if="getConstantValue(option) != null">{{ getConstantValue(option) }}</template>
+                <template v-else>
+                  <template v-for="value in getFormattedValues(option)">
+                    <span v-bind:key="value">{{ value }}</span>
+                  </template>
+                  <template v-if="getHasEnum(option)">
+                    <CollapsibleElement type="array" :initiallyCollapsed="true">
+                      <template v-slot:title><span>Enum:</span></template>
+                      <template v-slot:content>
+                        <span v-for="eVal in option.enum" v-bind:key="eVal">{{ isString ? ('"' + eVal + '"') : eVal.toString() }}</span>
+                      </template>
+                    </CollapsibleElement>
+                  </template>
+                  <template v-for="key in extraNestedElementKeys" >
+                    <span v-bind:key="key" v-if="key == 'pattern' && option[key] != null"><code>{{option[key]}}</code></span>
+                    <span v-bind:key="key" v-else-if="option[key] != null">{{option[key]}}</span>
+                  </template>
                 </template>
               </span>
             </div>
@@ -47,12 +66,23 @@
           <template v-for="condition in conditionalKeys">
             <div v-bind:key="condition" class="vueml-json-conditional" v-if="element[condition]">
               <span>{{ condition }}:</span>
-              <template v-for="value in getFormattedValues(element[condition])" >
-                <span v-bind:key="value">{{ value }}</span>
-              </template>
-              <template v-for="key in extraNestedElementKeys" >
-                <span v-bind:key="key" v-if="key == 'pattern' && element[condition][key] != null"><code>{{element[condition][key]}}</code></span>
-                <span v-bind:key="key" v-else-if="element[condition][key] != null">{{element[condition][key]}}</span>
+              <template v-if="getConstantValue(element[condition]) != null">{{ getConstantValue(element[condition]) }}</template>
+              <template v-else>
+                <template v-for="value in getFormattedValues(element[condition])">
+                  <span v-bind:key="value">{{ value }}</span>
+                </template>
+                <template v-if="getHasEnum(element[condition])">
+                  <CollapsibleElement type="array" :initiallyCollapsed="true">
+                    <template v-slot:title><span>Enum:</span></template>
+                    <template v-slot:content>
+                      <span v-for="eVal in element[condition].enum" v-bind:key="eVal">{{ isString ? ('"' + eVal + '"') : eVal.toString() }}</span>
+                    </template>
+                  </CollapsibleElement>
+                </template>
+                <template v-for="key in extraNestedElementKeys" >
+                  <span v-bind:key="key" v-if="key == 'pattern' && element[condition][key] != null"><code>{{element[condition][key]}}</code></span>
+                  <span v-bind:key="key" v-else-if="element[condition][key] != null">{{element[condition][key]}}</span>
+                </template>
               </template>
             </div>
           </template>
@@ -120,12 +150,7 @@ export default {
   },
   computed: {
     constantValue() {
-      // call to string for boolean defaults
-      var val = null
-      if (this.element.const != null) val = this.element.const.toString()
-      if (this.element.enum && this.element.enum.length == 1) val = this.element.enum[0].toString()
-      if (val && this.isString) val = '"' + val + '"'
-      return val
+      return this.getConstantValue(this.element)
     },
     baseNestedKeys() {
       return this.detailKeys.concat(this.combinationKeys).concat(this.conditionalKeys)
@@ -135,7 +160,7 @@ export default {
       this.baseNestedKeys.forEach((key) => {
         if (this.element[key] != null) nested = true
       })
-      return nested
+      return nested || this.getHasEnum(this.element)
     },
     nestedElementKeys() {
       return this.baseNestedKeys.concat(this.extraNestedElementKeys)
@@ -174,6 +199,17 @@ export default {
     }
   },
   methods: {
+    getConstantValue(element) {
+      // call to string for boolean defaults
+      var val = null
+      if (element.const != null) val = element.const.toString()
+      if (element.enum && element.enum.length == 1) val = element.enum[0].toString()
+      if (val && this.isString) val = '"' + val + '"'
+      return val
+    },
+    getHasEnum(element) {
+      return element.enum && element.enum.length > 0
+    },
     getFormattedValues(rules) {
       var values = []
       var value = null
